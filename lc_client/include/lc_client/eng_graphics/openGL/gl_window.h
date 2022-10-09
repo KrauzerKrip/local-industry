3fa3c885c3fb72c5422738eabc48a3a65f624b53
KrauzerKrip <krauzerkrip@gmail.com>
2022-10-09 22:34:20 +0300

    feat: add window class

---
+++ lc_client/include/lc_client/eng_graphics/openGL/gl_window.h
@@ -0,0 +1,11 @@
+#include "../../eng_graphics/i_window.h"
+
+class WindowGL : IWindow {
+public:
+
+	WindowGL();
+	~WindowGL();
+
+	void init();
+	void update();
+};
--- lc_client/src/eng_graphics/openGL/gl_window.cpp
+++ lc_client/src/eng_graphics/openGL/gl_window.cpp
@@ -1,1 +1,17 @@
-#include "lc_client/eng_graphics/i_window.h"
+#include "lc_client/eng_graphics/openGL/gl_window.h"
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+
+WindowGL::WindowGL(){}
+WindowGL::~WindowGL(){}
+
+void WindowGL::init() {
+
+}
+
+void WindowGL::update() {
+
+}
+
