69f181c7f03501a10c35ae9bfbf3b105443d80ef
KrauzerKrip <krauzerkrip@gmail.com>
2023-04-25 22:47:21 +0300

    feat: parent component

--- lc_client/src/lc_client/eng_scene/entt/components.h
+++ lc_client/src/lc_client/eng_scene/entt/components.h
@@ -25,6 +25,10 @@ struct Transform {
 	Transform(const Transform&) = default;
 };
 
+struct Parent {
+	entt::entity parent;
+};
+
 struct ModelData {
 	std::string packName;
 	std::string modelName;
