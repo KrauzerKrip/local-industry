1cdf4b83de601308188fca9733c479f5b1f83ef7
KrauzerKrip <krauzerkrip@gmail.com>
2023-06-07 12:30:00 +0300

    feat: add exception output in main function

--- game/main.cpp
+++ game/main.cpp
@@ -1,4 +1,4 @@
- #include "main.h"
+#include "main.h"
 
 #include <iostream>
 #include <string>
@@ -13,8 +13,8 @@
 int main() {
 
 	std::string title = "Local` Engine";
-	//int width = 1080;
-	//int height = 720;
+	// int width = 1080;
+	// int height = 720;
 	int width = 1920;
 	int height = 1080;
 	bool vSync = true;
@@ -22,25 +22,43 @@ int main() {
 	int targetFPS = 60;
 	int targetUPS = 60;
 
-	Tier0* pTier0 = new Tier0();
+	Tier0* pTier0 = nullptr;
 
-	IWindow* pWindow = new WindowGL(title, width, height, new int[2]{16, 9}, vSync, 45.0);
-	IGameLogic* pGameLogic = new Game(pWindow, pTier0);
-	Loop* pLoop = Loop::createInstance(pWindow, pGameLogic, targetFPS, targetUPS);
+	IWindow* pWindow = nullptr;
+	IGameLogic* pGameLogic = nullptr;
+	Loop* pLoop = nullptr;
 
-	pLoop->init();
+	try {
+		Tier0* pTier0 = new Tier0();
 
-	pLoop->startLoop();
+		IWindow* pWindow = new WindowGL(title, width, height, new int[2]{16, 9}, vSync, 45.0);
+		IGameLogic* pGameLogic = new Game(pWindow, pTier0);
+		Loop* pLoop = Loop::createInstance(pWindow, pGameLogic, targetFPS, targetUPS);
 
-	pLoop->cleanUp();
-	pWindow->terminate(); // mb in loop
+		pLoop->init();
+		pLoop->startLoop();
 
-	delete pLoop;
-	delete pWindow;
-	delete pGameLogic;
-	delete pTier0;
-	
+		pLoop->cleanUp();
+		pWindow->terminate(); // mb in loop
+	}
+	catch (std::runtime_error& exception) {
+		std::cerr << "Exception: " << exception.what() << std::endl;
+		exit(-1);
+	}
+
+	if (pLoop) {
+		delete pLoop;
+	}
+
+	if (pWindow) {
+		delete pWindow;
+	}
+	if (pGameLogic) {
+		delete pGameLogic;
+	}
+	if (pTier0) {
+		delete pTier0;
+	}
+
+	exit(0);
 }
-
-
-
