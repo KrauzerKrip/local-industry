f7a19405edd40e80bd883fc01a26a85935c77b4e
KrauzerKrip <krauzerkrip@gmail.com>
2023-06-12 17:10:40 +0300

    fix: fix rotations

--- lc_client/src/lc_client/eng_graphics/openGL/gl_render.cpp
+++ lc_client/src/lc_client/eng_graphics/openGL/gl_render.cpp
@@ -187,7 +187,9 @@ void RenderGL::setDependecies(Scene* pScene, Skybox* pSkybox) {
 
 void RenderGL::transform(glm::mat4& model, Transform& transform) {
 	model = glm::translate(model, transform.position);
-	model = glm::rotate(model, glm::radians(-55.0f), transform.rotation);
+	model = glm::rotate(model, glm::radians(transform.rotation.x), glm::vec3(1, 0, 0));
+	model = glm::rotate(model, glm::radians(transform.rotation.y), glm::vec3(0, 1, 0));
+	model = glm::rotate(model, glm::radians(transform.rotation.z), glm::vec3(0, 0, 1));
 	model = glm::scale(model, transform.scale);
 }
 
