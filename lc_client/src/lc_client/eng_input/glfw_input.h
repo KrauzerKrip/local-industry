736917cfb4896053774d288ebff260325ccec7bf
KrauzerKrip <krauzerkrip@gmail.com>
2023-12-14 20:14:36 +0200

    fix: input mapped callbacks

--- lc_client/src/lc_client/eng_input/glfw_input.h
+++ lc_client/src/lc_client/eng_input/glfw_input.h
@@ -29,7 +29,7 @@ private:
 	std::unordered_map<KeyCode, int> m_keyMap;
 	
 	std::vector<std::function<void(KeyCode)>> m_keyCallbacks;
-	std::unordered_map<KeyCode, std::function<void()>> m_mappedKeyCallbacks;
+	std::unordered_map<KeyCode, std::vector<std::function<void()>>> m_mappedKeyCallbacks;
 	std::vector<std::function<void(glm::vec2)>> m_mouseCallbacks;
 	std::vector<std::function<void(glm::vec2)>> m_mouseWheelCallbacks;
 
