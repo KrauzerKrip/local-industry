653228b37c5eb9474be1505de212a572349b6a66
KrauzerKrip <krauzerkrip@gmail.com>
2024-05-23 19:02:15 +0300

    fix: trade menu controller

--- game/src/game/gui/trade_menu/trade_menu_controller.cpp
+++ game/src/game/gui/trade_menu/trade_menu_controller.cpp
@@ -12,6 +12,8 @@
 TradeMenuController::TradeMenuController(TradeMenuView* pView, entt::registry* pRegistry) {
 	m_pView = pView;
 	m_pRegistry = pRegistry;
+
+	m_isViewOpenFirstTime = true;
 }
 
 void TradeMenuController::input() { 
@@ -29,61 +31,77 @@ void TradeMenuController::input() {
 	
 	entt::entity traderEntity = entt::null;
 
-	std::vector<OfferData> purchaseData;
-	std::vector<OfferData> salesData;
 	for (auto&& [entity, trader] : selectedTraders.each()) {
 		traderEntity = entity;
-		for (auto [item, price] : trader.purchaseOffers) {
-			OfferData data;
-			data.iconPath = "gmod_vibe";
-			data.label = m_pRegistry->get<Item>(item).name;
-			data.priceLabel = std::to_string(price);
-			data.buttonLabel = "Buy";
-			data.callback = [this, entity, item]() { 
-				auto view = m_pRegistry->view<GameCharacter>();
-				for (auto character : view) {
-					m_pRegistry->emplace<PurchaseRequest>(character, PurchaseRequest(entity, item, 1));
-				}
-				};
-			if (price <= account.funds) {
-				data.affordable = true;
-			}
-			else {
-				data.affordable = false;
-			}
-			purchaseData.push_back(data);
-		}
-		for (auto [item, price] : trader.saleOffers) {
-			OfferData data;
-			data.iconPath = "gmod_vibe";
-			data.label = m_pRegistry->get<Item>(item).name;
-			data.priceLabel = std::to_string(price);
-			data.buttonLabel = "Sell";
-			data.callback = [this, entity, item]() {
-				auto view = m_pRegistry->view<GameCharacter>();
-				for (auto character : view) {
-					m_pRegistry->emplace<SaleRequest>(character, SaleRequest(entity, item, 1));
-				}
-			};
-			data.affordable = false;
-			auto view = m_pRegistry->view<GameCharacter, Inventory>();
-			for (auto [ent, character, inventory] : view.each()) {
-				if (inventory.items.find(item) != inventory.items.end()) {
-					if (1 <= inventory.items.at(item)) {
-						data.affordable = true;
-					}
-				}
-			}
-
-			salesData.push_back(data);
-		}
 	}
 
 	if (m_pRegistry->valid(traderEntity)) {
-		m_pView->setPurchasesData(purchaseData);
-		m_pView->setSalesData(salesData);
 		m_pView->showWithChildren();
 		m_pRegistry->remove<Selected>(traderEntity);
+		m_lastTrader = traderEntity;
 	}
 
+	if (m_pView->isVisible()) {
+		std::vector<OfferData> purchaseData;
+		std::vector<OfferData> salesData;
+
+		auto view = m_pRegistry->view<GameCharacter>();
+		for (auto character : view) { // TODO remake for multiple characters
+			this->setOffersData(m_lastTrader, account, purchaseData, salesData, character);
+		}
+
+		m_pView->setPurchasesData(purchaseData);
+		m_pView->setSalesData(salesData);
+
+		if (m_isViewOpenFirstTime) {
+			m_pView->show();
+			m_isViewOpenFirstTime = false;
+		}
+	}
+
+}
+
+void TradeMenuController::setOffersData(entt::entity traderEntity, PlayerAccount& account,
+	std::vector<OfferData>& purchaseData, std::vector<OfferData>& salesData, entt::entity buyerCharacter) {
+	
+	Trader& trader = m_pRegistry->get<Trader>(traderEntity);
+
+	for (auto [item, price] : trader.purchaseOffers) {
+		OfferData data;
+		data.iconPath = "gmod_vibe";
+		data.label = m_pRegistry->get<Item>(item).name;
+		data.priceLabel = std::to_string(price); 
+		data.buttonLabel = "Buy";
+		data.callback = [this, traderEntity, &account, buyerCharacter, item]() {
+			m_pRegistry->emplace<PurchaseRequest>(buyerCharacter, PurchaseRequest(traderEntity, item, 1));
+		};
+		if (price <= account.funds) {
+			data.affordable = true;
+		}
+		else {
+			data.affordable = false;
+		}
+		purchaseData.push_back(data);
+	}
+	for (auto [item, price] : trader.saleOffers) {
+		OfferData data;
+		data.iconPath = "gmod_vibe";
+		data.label = m_pRegistry->get<Item>(item).name;
+		data.priceLabel = std::to_string(price);
+		data.buttonLabel = "Sell";
+		data.callback = [this, traderEntity, &account, buyerCharacter, item]() {
+			m_pRegistry->emplace<SaleRequest>(buyerCharacter, SaleRequest(traderEntity, item, 1));
+		};
+		data.affordable = false;
+		auto view = m_pRegistry->view<GameCharacter, Inventory>();
+		for (auto [ent, character, inventory] : view.each()) {
+			if (inventory.items.find(item) != inventory.items.end()) {
+				if (1 <= inventory.items.at(item)) {
+					data.affordable = true;
+				}
+			}
+		}
+
+		salesData.push_back(data);
+	}
 }
--- game/src/game/gui/trade_menu/trade_menu_controller.h
+++ game/src/game/gui/trade_menu/trade_menu_controller.h
@@ -3,6 +3,7 @@
 #include <entt/entt.hpp>
 
 #include "trade_menu_view.h"
+#include "game/economy/components.h"
 
 
 class TradeMenuController {
@@ -12,6 +13,10 @@ public:
 	void input();
 
 private:
+	void setOffersData(entt::entity traderEntity, PlayerAccount& account, std::vector<OfferData>& purchaseData, std::vector<OfferData>& salesData, entt::entity buyer);
+
 	TradeMenuView* m_pView = nullptr;
 	entt::registry* m_pRegistry = nullptr;
+	entt::entity m_lastTrader = entt::null;
+	bool m_isViewOpenFirstTime;
 };
